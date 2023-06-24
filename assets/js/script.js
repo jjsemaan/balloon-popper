15022dce8b8fc8bfad04939d5d25aab9afd48ce8
jjsemaan <>
2023-06-24 09:28:56 +0000

    Modify js to creat game levels

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,33 +7,110 @@ For this game css and js are separated into styles.css and script.js files
 Improvised code will be prefixed 'Improvised' in a comment 
 */
 
-const balloon = document.getElementById('balloon'); 
-const scoreBox = document.getElementById('score-box'); 
+// Select the balloon elements and the score box element from the DOM
+const redBalloon = document.getElementById('red-balloon');
+const blueBalloon = document.getElementById('blue-balloon');
+const yellowBalloon = document.getElementById('yellow-balloon');
+const greenBalloon = document.getElementById('green-balloon');
+const scoreBox = document.getElementById('score-box');
+
+// Click events of the balloons
+redBalloon.addEventListener('click', poppedBalloon);
+blueBalloon.addEventListener('click', poppedBalloon);
+yellowBalloon.addEventListener('click', poppedBalloon);
+greenBalloon.addEventListener('click', poppedBalloon);
 
 
 // Action when the balloon is clicked
-balloon.addEventListener('click', clickedIt);
 let score = 0;
 let count = 0;
+let level = 'easy'; // Start with the easy level
 
-function clickedIt() {
-    score++;
-    scoreBox.innerHTML = 'score: ' + score;
-    balloon.style.height = '1px';
-}
+// Display the initial score and level
+scoreBox.innerHTML = 'LEVEL: ' + level + ' - SCORE: ' + score;
 
-// Make the balloon move on the screen
-function moveIt() {
-    balloon.style.height = '160px';
-    balloon.style.left = Math.random() * 800 + 'px';
-    balloon.style.top = Math.random() * 600 + 'px';
+// Function to handle the click event on the balloons
+function poppedBalloon() {
+    if (level === 'easy') {
+        if (this === redBalloon) {
+            score--; // Decrement the score if red balloon is clicked
+        } else {
+            score++; // Increment the score for other balloons
+        }
+    } else if (level === 'medium') {
+        if (this === redBalloon) {
+            score--; // Decrement the score if red balloon is clicked
+        } else {
+            score++; // Increment the score for blue and yellow balloons
+        }
+    } else if (level === 'hard') {
+        if (this === redBalloon) {
+            score--; // Decrement the score if red balloon is clicked
+        } else {
+            score++; // Increment the score for blue, green, and yellow balloons
+        }
+    }
 
-    count++;
-    if (count === 20) {
-        alert('Game Over!');
-        clearInterval(timer);
+    scoreBox.innerHTML = 'SCORE: ' + score; // Update the score display
+    this.style.height = '1px'; // Shrink the balloon on click
+
+    // Check the current level and proceed accordingly
+    if (level === 'easy' && count >= 10) {
+        if (score > 1) {
+            level = 'medium';
+            alert('Congratulations! You have advanced to the Medium level.');
+            blueBalloon.style.display = 'none'; // Hide the blue balloon in medium level
+        } else {
+            gameOver();
+        }
+    } else if (level === 'medium' && count >= 10) {
+        if (score > 1) {
+            level = 'hard';
+            alert('Congratulations! You have advanced to the Hard level.');
+            yellowBalloon.style.display = 'none'; // Hide the yellow balloon in hard level
+        } else {
+            gameOver();
+        }
+    } else if (level === 'hard' && count >= 10) {
+        if (score > 1) {
+            alert('Congratulations! You have won the game!');
+        } else {
+            gameOver();
+        }
     }
 }
 
-let timer = setInterval(moveIt, 1000);
+// Function to handle game over
+function gameOver() {
+    alert('Game Over!');
+    clearInterval(timer); // Stop the balloon movement
+}
 
+// Function to move the balloons on the screen
+function moveBalloon() {
+    // Reset the balloon sizes and positions
+    redBalloon.style.height = '160px';
+    blueBalloon.style.height = '160px';
+    yellowBalloon.style.height = '160px';
+    greenBalloon.style.height = '160px';
+
+    redBalloon.style.left = Math.random() * 800 + 'px';
+    blueBalloon.style.left = Math.random() * 800 + 'px';
+    yellowBalloon.style.left = Math.random() * 800 + 'px';
+    greenBalloon.style.left = Math.random() * 800 + 'px';
+
+    redBalloon.style.top = Math.random() * 600 + 'px';
+    blueBalloon.style.top = Math.random() * 600 + 'px';
+    yellowBalloon.style.top = Math.random() * 600 + 'px';
+    greenBalloon.style.top = Math.random() * 600 + 'px';
+
+    count++; // Increment the count of balloon movements
+
+    // Check if the count reaches the maximum limit (10)
+    if (count === 10) {
+        gameOver();
+    }
+}
+
+// Set an interval to move the balloons every 1000 milliseconds (1 second)
+let timer = setInterval(moveBalloon, 1000);
