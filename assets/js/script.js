99f641a4450f53698b081b5eea81f6ab19ffa797
jjsemaan <>
2023-06-25 12:24:16 +0000

    Add basic game code that moves balloons

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,9 +23,9 @@ document.getElementById('game-form').addEventListener('submit', function (event)
 
     // Data Structure to map difficulty level
     var levelToHtml = new Map([
-        ['easy', 'easy.html'],      //Map easy level to easy.html
-        ['medium', 'medium.html'],  //Map medium level to medium.html
-        ['hard', 'hard.html']       //Map hard level to hard.html
+        ['easy', 'easy.html'], //Map easy level to easy.html
+        ['medium', 'medium.html'], //Map medium level to medium.html
+        ['hard', 'hard.html'] //Map hard level to hard.html
     ]);
 
     // Retrieve the HTML file based on the selected difficulty level
@@ -35,7 +35,6 @@ document.getElementById('game-form').addEventListener('submit', function (event)
     window.location.href = htmlFile;
 });
 
-
 // Action when the balloon is clicked
 let score = 0;
 let count = 0;
@@ -44,14 +43,7 @@ let count = 0;
 let scoreLabel = document.getElementById('score-label');
 
 //display the initial score as 0
-scoreLabel.textContent = 'Score: ' + score; 
-
-    
-// Select the start game button element from the DOM
-const startButton = document.getElementById('start-button');
-
-// Add click event to the start game button
-startButton.addEventListener('click', startGame);
+scoreLabel.textContent = 'Score: ' + score;
 
 // Select the balloon elements and the score box element from the DOM
 const redBalloon = document.getElementById('red-balloon');
@@ -65,22 +57,43 @@ redBalloon.addEventListener('click', poppedBalloon);
 blueBalloon.addEventListener('click', poppedBalloon);
 yellowBalloon.addEventListener('click', poppedBalloon);
 greenBalloon.addEventListener('click', poppedBalloon);
-    
 
-
-    
-
-/*
-    //Target each html page separately
-if (window.location.pathname === '/easy.html') {
-    // Code specific to easy.html
-} else if (window.location.pathname === '/medium.html') {
-    // Code specific to medium.html
-} else if (window.location.pathname === '/hard.html') {
-    // Code specific to hard.html
+function poppedBalloon() {
+    score++;
+    scoreLabel.innerHTML = 'Score: ' + score;
+    blueBalloon.style.height = '1px';
+    greenBalloon.style.height = '1px';
+    yellowBalloon.style.height = '1px';
+    redBalloon.style.height = '1px';
 }
-*/
 
+function moveBalloon() {
+    // Reset the balloon sizes and positions
+    redBalloon.style.height = '200px';
+    blueBalloon.style.height = '200px';
+    yellowBalloon.style.height = '200px';
+    greenBalloon.style.height = '200px';
 
+    redBalloon.style.left = Math.random() * 800 + 'px';
+    blueBalloon.style.left = Math.random() * 800 + 'px';
+    yellowBalloon.style.left = Math.random() * 800 + 'px';
+    greenBalloon.style.left = Math.random() * 800 + 'px';
 
+    redBalloon.style.top = Math.random() * 600 + 'px';
+    blueBalloon.style.top = Math.random() * 600 + 'px';
+    yellowBalloon.style.top = Math.random() * 600 + 'px';
+    greenBalloon.style.top = Math.random() * 600 + 'px';
 
+    count++; // Increment the count of balloon movements
+    if (count == 10) {
+        alert('Game Over!');
+        clearInterval(timer);
+    }
+}
+
+let timer = setInterval(moveBalloon, 1000);
+
+// Select the start game button element from the DOM
+const startButton = document.getElementById('start-button');
+// Add click event to the start game button
+startButton.addEventListener('click', startGame);
