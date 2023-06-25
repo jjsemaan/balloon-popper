255decb55bdfd7109ae41c104abe011b1fcad67c
jjsemaan <>
2023-06-25 10:34:52 +0000

    Add data structure to js mapping navigation to corresponding page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,38 @@
+// Execute the following code when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
+    // Retrieve the nickname from localStorage
     var nickname = localStorage.getItem('nickname');
     if (nickname) {
+        // Display the nickname on the page
         var nicknameElement = document.getElementById('nickname-display');
         nicknameElement.textContent = 'Player Nickname: ' + nickname + '!';
     }
 });
 
+// Add event listener to the form submission
 document.getElementById('game-form').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent form submission
+
+    // Retrieve the nickname from the input field
     var nickname = document.getElementById('nickname').value;
-    localStorage.setItem('nickname', nickname); // Store the nickname in localStorage
-    window.location.href = 'easy.html'; // Redirect to easy.html
+    // Store the nickname in localStorage
+    localStorage.setItem('nickname', nickname);
+
+    // Retrieve the selected difficulty level from the dropdown menu
+    var level = document.getElementById('level').value;
+
+    // Data Structure to map difficulty level
+    var levelToHtml = new Map([
+        ['easy', 'easy.html'],      //Map easy level to easy.html
+        ['medium', 'medium.html'],  //Map medium level to medium.html
+        ['hard', 'hard.html']       //Map hard level to hard.html
+    ]);
+
+    // Retrieve the HTML file based on the selected difficulty level
+    var htmlFile = levelToHtml.get(level);
+
+    // Redirect the page to the corresponding HTML file
+    window.location.href = htmlFile;
 });
+
 
